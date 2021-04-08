dc6eabe72203c8e3370d1f0da1eac6fe128fb647
Kevelly <kevelly133@gmail.com>
2021-04-07 23:03:01 -0300

    Atividade POO 07.04

---
+++ POO/src/Automovel.java
@@ -0,0 +1,15 @@
+
+public class Automovel {
+// declaração dos atributos da classe
+	
+	private String nomeProprietario;
+	private String modelo;
+	private String placa;
+	private int ano;
+	
+	// metodo construtor
+	
+	public Automovel(String nomeProprietario,String modelo,String placa, int ano)
+	
+	
+}
---
+++ POO/src/Automoveltest.java
@@ -0,0 +1,9 @@
+
+public class Automoveltest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Automovel auto1 = new Automovel ("Thiago ", )
+	}
+
+}
